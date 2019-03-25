11bc3ba753ce005b28ce6f32959f553f98650ab0
llach <llach@techfak.uni-bielefeld.de>
2019-03-25 19:46:11 +0100

    implemented rendering and plotting for pendulum vis envs

--- gym/envs/classic_control/pendulum_test.py
+++ gym/envs/classic_control/pendulum_test.py
@@ -1,6 +1,9 @@
 import gym
+import cv2
 import numpy as np
 import cairocffi as cairo
+
+from skimage.transform import resize
 
 
 class PendulumTestEnv(gym.Env):
@@ -16,10 +19,14 @@ class PendulumTestEnv(gym.Env):
         self.thetas = np.linspace(0, 2*np.pi, self.steps)
         self.t = 0
 
+        self.frame_t = None
+        self.has_window = False
+
     def _get_theta(self):
         return self.thetas[self.t]
 
     def step(self, a):
+
         if self.t < self.steps-1:
             self.t += 1
             d = False
@@ -32,6 +39,23 @@ class PendulumTestEnv(gym.Env):
     def reset(self):
         self.t = 0
         return self._get_obs()
+
+    def render(self, mode=''):
+        if self.frame_t is None: return
+
+        if not self.has_window:
+            cv2.namedWindow('pendulum')
+
+        img = resize(self.frame_t.copy(), [512, 512], mode='reflect', anti_aliasing=True)
+        print(img.shape, img.dtype)
+        cv2.putText(img, 'theta {:.3}'.format(angle_normalize(self._get_theta())), (20, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, .5, 2)
+
+        cv2.putText(img, '~costs {:.3}'.format(angle_normalize(self._get_theta())**2), (250, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, .5, 2)
+
+        cv2.imshow('pendulum', img)
+        cv2.waitKey(1)
 
     def _render_pendulum(self):
         cr = cairo.Context(self.surf)
@@ -62,7 +86,9 @@ class PendulumTestEnv(gym.Env):
         return np.expand_dims(np.dot(np.frombuffer(self.surf.get_data(), np.uint8).reshape([self.w, self.h, 4])[..., :3], [0.299, 0.587, 0.114]), -1)/255
 
     def _get_obs(self):
-        return self._render_pendulum()
+        self.frame_t = self._render_pendulum()
+        return self.frame_t
+
 
 def angle_normalize(x):
     return (((x+np.pi) % (2*np.pi)) - np.pi)
--- gym/envs/classic_control/pendulum_visual.py
+++ gym/envs/classic_control/pendulum_visual.py
@@ -1,6 +1,7 @@
+import cv2
 import gym
 import cairocffi as cairo
-
+from skimage.transform import resize
 
 from gym import spaces
 from gym.utils import seeding
@@ -15,11 +16,14 @@ class PendulumVisualEnv(gym.Env):
         self.dt=.05
 
         # rendering things
-        self.w , self.h = 64, 64
+        self.w, self.h = 64, 64
         self.surf = cairo.ImageSurface(cairo.FORMAT_RGB24, self.w, self.h)
 
         self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(1,), dtype=np.float32)
         self.observation_space = spaces.Box(low=0, high=1, shape=(64, 64, 1), dtype=np.float32)
+        self.frame_t = None
+        self.has_window = False
+        self.costs = 0
 
         self.seed()
 
@@ -38,6 +42,7 @@ class PendulumVisualEnv(gym.Env):
         u = np.clip(u, -self.max_torque, self.max_torque)[0]
         self.last_u = u # for rendering
         costs = angle_normalize(th)**2 + .1*thdot**2 + .001*(u**2)
+        self.costs = float(costs)
 
         newthdot = thdot + (-3*g/(2*l) * np.sin(th + np.pi) + 3./(m*l**2)*u) * dt
         newth = th + newthdot*dt
@@ -51,6 +56,22 @@ class PendulumVisualEnv(gym.Env):
         self.state = self.np_random.uniform(low=-high, high=high)
         self.last_u = None
         return self._get_obs()
+
+    def render(self, mode=''):
+        if self.frame_t is None: return
+
+        if not self.has_window:
+            cv2.namedWindow('pendulum')
+
+        img = resize(self.frame_t.copy(), [512, 512], mode='reflect', anti_aliasing=True)
+        cv2.putText(img, 'theta {:.3}'.format(angle_normalize(self._get_theta())), (20, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, .5, 2)
+
+        cv2.putText(img, 'costs {:.3}'.format(self.costs), (250, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, .5, 2)
+
+        cv2.imshow('pendulum', img)
+        cv2.waitKey(1)
 
     def _render_pendulum(self):
         cr = cairo.Context(self.surf)
@@ -81,7 +102,11 @@ class PendulumVisualEnv(gym.Env):
         return np.expand_dims(np.dot(np.frombuffer(self.surf.get_data(), np.uint8).reshape([self.w, self.h, 4])[..., :3], [0.299, 0.587, 0.114]), -1)/255
 
     def _get_obs(self):
-        return self._render_pendulum()
+        self.frame_t = self._render_pendulum()
+        return self.frame_t
+
+    def _get_theta(self):
+        return float(self.state[0])
 
 def angle_normalize(x):
     return (((x+np.pi) % (2*np.pi)) - np.pi)
